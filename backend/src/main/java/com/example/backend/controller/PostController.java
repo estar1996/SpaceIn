58073359d2e3007e1567096c262237ad2d416ca4
estar1996 <estar19961996@gmail.com>
2023-05-12 13:11:00 +0900

    feat: add Post api

--- backend/src/main/java/com/example/backend/controller/PostController.java
+++ backend/src/main/java/com/example/backend/controller/PostController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.domain.Post;
 import com.example.backend.dto.PostDto;
 import com.example.backend.dto.PostResponseDto;
+import com.example.backend.repository.PostRepository;
 import com.example.backend.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,7 +18,7 @@ public class PostController {
 
     @Autowired
     private PostService postService;
-
+    private PostRepository postRepository;
     @PostMapping
     public ResponseEntity<PostResponseDto> createPost(@RequestBody PostDto postDto) {
         PostResponseDto newPost = postService.createPost(postDto, postDto.getUserId());
@@ -29,8 +30,15 @@ public class PostController {
         PostResponseDto post = postService.getPost(id);
         return new ResponseEntity<>(post, HttpStatus.OK);
     }
+    @GetMapping
+    public List<PostResponseDto> getAllPosts() {
+        return postService.getAllPosts();
+    }
 
-
+    @GetMapping("/user/{userId}")
+    public List<PostResponseDto> getPostsByUserId(@PathVariable Long userId) {
+        return postService.getPostsByUserId(userId);
+    }
     @GetMapping("/{id}/near")
     public ResponseEntity<List<PostResponseDto>> getNearbyPosts(@RequestParam("latitude") double userLatitude, @RequestParam("longitude") double userLongitude) {
         List<PostResponseDto> nearbyPosts = postService.getNearbyPost(userLatitude, userLongitude, 5.0);
